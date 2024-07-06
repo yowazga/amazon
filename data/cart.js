9b4024c0f50afbc0417d068364a76cf76cb408c1
Younes Wazga <yowazga@c1r8p8.1337.ma>
2024-07-06 10:53:45 +0100

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export const cart = [];
+
+export function addToCart(productId) {
+	let matchigItem;
+
+	cart.forEach((cartItem) => {
+		if (productId === cartItem.productId)
+			matchigItem = cartItem;
+	});
+
+	if (matchigItem)
+		matchigItem.quantity++;
+	else {
+		cart.push({
+			productId: productId,
+			quantity: 1
+		});
+	}
+}
