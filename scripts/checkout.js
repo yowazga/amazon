44bf4e0d99618835bade825575af862eeb9361ee
Younes Wazga <yowazga@c1r8p8.1337.ma>
2024-07-16 10:19:48 +0100

    Creat oop versionn of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrdferSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js'
 
 
 renderOrdferSummary();
